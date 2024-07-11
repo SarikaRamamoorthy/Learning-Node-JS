ebf80da6ae648908c4c61c7aff32508a0563a198
SarikaRamamoorthy <myothermailid001@gmail.com>
2024-07-11 09:55:20 +0530

    final commit

--- index.js
+++ index.js
@@ -1,20 +1,42 @@
+const http = require('http');
 const fs = require('fs');
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
+const url = require('url');
+const replaceTemplate = require('./modules/replaceTemplate');
 
+const templateOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const templateCards = fs.readFileSync(`${__dirname}/templates/template-cards.html`, 'utf-8');
+const templateProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
 const dataObj = JSON.parse(data);
 
-const http = require('http');
 const server = http.createServer((req, res) => {
+    /*
     const path = req.url;
-    if(path === '/' || path === '/overview'){
+    console.log(path);
+    console.log(url.parse(req.url, true));
+    url module is used to parse the url
+    */
+    const {pathname, query} = url.parse(req.url, true);
+    if(pathname === '/' || pathname === '/overview'){
         // overview
-        res.end("Overview");
+        res.writeHead(200, {
+            'Content-type' : 'text/html'
+        }); 
+        const cardsHtml = dataObj.map(el => replaceTemplate(templateCards, el)).join('');
+        const output = templateOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+        res.end(output);
     }
-    else if(path === '/product') {
+    else if(pathname === '/product') {
         // product
-        res.end("Product page");
+        res.writeHead(200, {
+            'Content-type' : 'text/html'
+        }); 
+        const product = dataObj[query.id];
+        const output = replaceTemplate(templateProduct, product);
+        res.end(output);
     }
-    else if(path === '/api') {
+    else if(pathname === '/api') {
         res.writeHead(200, {
             'Content-type':'application/json'
         });
@@ -27,5 +49,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(8000,'127.0.0.1',() => {
-    console.log("Server listening on port 8080");
+    console.log("Server listening on port 8000");
 });
